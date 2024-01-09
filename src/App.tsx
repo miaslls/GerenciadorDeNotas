9cdecee523f822d1b315948a3895583bc74fdfbd
miaslls <97785569+miaslls@users.noreply.github.com>
2024-01-09 01:29:49 -0300

    refactor useResultados to useResultadosByBimestre

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,28 @@ import './styles/global.css';
 import { Tooltip } from 'react-tooltip';
 import { Toaster } from 'react-hot-toast';
 import { Bimestre } from './api/types/Resultado';
-import { useModal } from './components/modal/ModalContextProvider';
+import { useResultadoModal } from './components/modal/useResultadoModal';
+import { useResultadosGroupedByBimestre } from './api/resultados/useResultados';
 import BimestreSection from './components/page/BimestreSection';
 import ResultadoModal from './components/modal/ResultadoModal';
 
 function App() {
-  const { modalState, handleModalState } = useModal();
+  const { modalState, handleModalState } = useResultadoModal();
+
+  const {
+    resultadosGroupedByBimestre: resultados,
+    isLoading,
+    error,
+  } = useResultadosGroupedByBimestre();
+
+  if (isLoading) {
+    return <div>Carregando...</div>;
+  }
+
+  if (error) {
+    console.error(error);
+    return <div>Falha ao carregar resultados</div>;
+  }
 
   return (
     <>
@@ -27,6 +43,7 @@ function App() {
       <main className="page">
         {Object.values(Bimestre).map((bimestre) => (
           <BimestreSection
+            resultados={resultados[bimestre]}
             bimestre={bimestre}
             openModal={handleModalState}
             key={`bimestre-${bimestre}`}
@@ -38,6 +55,7 @@ function App() {
         <ResultadoModal
           closeModal={handleModalState}
           bimestre={modalState.bimestre}
+          resultados={resultados[modalState.bimestre]}
         />
       )}
     </>
--- src/api/resultados/useResultados.ts
+++ src/api/resultados/useResultados.ts
@@ -3,26 +3,30 @@ import { fetcher } from '../helpers/fetcher';
 import { endpoints } from '../helpers/endpoints';
 import { Bimestre, Resultado } from '../types/Resultado';
 
-export function useResultados(bimestre?: Bimestre) {
+export function useResultadosGroupedByBimestre() {
   const {
     data: resultados,
     isLoading,
     error,
-  } = useSWR(endpoints.getResultados, fetcher);
+  } = useSWR<Resultado[]>(endpoints.getResultados, fetcher);
 
-  if (!bimestre) {
-    return {
-      resultados,
-      isLoading,
-      isError: error,
-    };
+  const groupedBimestres = Object.values(Bimestre).map(
+    (bimestre) => [bimestre, []] as [Bimestre, Resultado[]]
+  );
+
+  const resultadosGroupedByBimestre = Object.fromEntries(
+    groupedBimestres
+  ) as Record<Bimestre, Resultado[]>;
+
+  if (resultados) {
+    for (const resultado of resultados) {
+      resultadosGroupedByBimestre[resultado.bimestre].push(resultado);
+    }
   }
 
   return {
-    resultadosByBimestre: resultados?.filter(
-      (resultado: Resultado) => resultado.bimestre === bimestre
-    ),
+    resultadosGroupedByBimestre,
     isLoading,
-    isError: error,
+    error,
   };
 }
